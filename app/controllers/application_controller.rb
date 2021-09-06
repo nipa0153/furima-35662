a0a882fdb7db0e72d5d041e2110f25eca8dec7f1
nipa0153 <0vx38c1620m459e@gmail.com>
2021-09-06 20:23:42 +0900

    カラムの保存設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_aucion :authenticate_user!
-  
+  before_action :authenticate_user!
+  before_action :cofigure_permitted_parameters, if: :devise_controller?
+
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_kana, :first_kana, :birthday ])
 end
