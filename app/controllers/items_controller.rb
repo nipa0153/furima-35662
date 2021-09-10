63febb37e6dccf5258dea11c4d2bef566d30948c
nipa0153 <0vx38c1620m459e@gmail.com>
2021-09-10 21:52:24 +0900

    エラーメッセージの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,5 +2,24 @@ class ItemsController < ApplicationController
 
   def index
   end
-  
+
+  def new
+    
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to action: :index
+    else
+      render action: :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:nickname, :email, :encrypted_password, :last_name, :first_name, :last_kana, :first_kana, :birthday)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  validates :nickname,    presence: true
-  validates :last_name,   presence: true
-  validates :first_name,  presence: true
-  validates :last_kana,   presence: true
-  validates :first_kana,  presence: true
-  validates :birthday,    presence: true
+
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+
+    with_options format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."} do
+      validates :last_name
+      validates :first_name
+    end
+
+    with_options format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input fullwidth katakana characters."} do
+      validates :last_kana
+      validates :first_kana
+    end 
+  end
 
   has_many :items
   has_many :buyers  
